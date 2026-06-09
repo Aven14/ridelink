0fd9d2eac0b4e771e191b6ebf71934b812df9af5
Aven14 <aven14a@gmail.com>
2026-06-09 20:06:09 +0200

    Update

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL || "postgresql://dummy:dummy@ep-dummy-123456.eu-central-1.aws.neon.tech/neondb?sslmode=require");
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
 
 export type DB = typeof db;
